87be7d4d6990e88f64e1d2b7bfcd3c595c9096cb
pablomacielcrypto <81847772+pablomacielcrypto@users.noreply.github.com>
2021-08-12 05:23:35 -0300

    Confeccion de Suma.XML

--- app/src/main/java/com/example/tpn1/Ejercicio_1.java
+++ app/src/main/java/com/example/tpn1/Ejercicio_1.java
@@ -9,6 +9,6 @@ public class Ejercicio_1 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.ejercicio1);
+        setContentView(R.layout.suma);
     }
 }
