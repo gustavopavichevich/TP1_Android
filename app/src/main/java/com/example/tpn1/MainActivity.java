a3b048674a8faa982a9da497e95fe3569d93e92e
pablomacielcrypto <81847772+pablomacielcrypto@users.noreply.github.com>
2021-08-12 03:07:58 -0300

    inicio de TP1

---
+++ app/src/main/java/com/example/tpn1/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.tpn1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    //Metodo del boton ejercicio 1, suma.
+
+    public void Ejer_1 (View view)
+    {
+        Intent ejer_1 = new Intent( this, Ejercicio_1.class);
+        startActivity(ejer_1 );
+    }
+    public void Ejer_2 (View view)
+    {
+        Intent ejer_2 = new Intent( this, Calculadora.class);
+        startActivity(ejer_2 );
+    }
+}
