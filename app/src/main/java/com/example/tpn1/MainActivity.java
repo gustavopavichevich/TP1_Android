efa3048d0106cc4966a97b426978d31c3e596021
LeoYermoli <81972081+LeoYermoli@users.noreply.github.com>
2021-09-03 00:18:19 -0300

    Pantalla principal . menu y pantalla de agregar

--- app/src/main/java/com/example/tpn1/MainActivity.java
+++ app/src/main/java/com/example/tpn1/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,14 +19,37 @@ public class MainActivity extends AppCompatActivity {
 
     public void Ejer_1 (View view)
     {
-        Intent ejer_1 = new Intent( this, Ejercicio_1.class);
+        Intent ejer_1 = new Intent( this, agregar.class);
         startActivity(ejer_1 );
     }
     //Metodo del boton ejercicio 2, Calculadora.
 
     public void Ejer_2 (View view)
     {
-        Intent ejer_2 = new Intent( this, Calculadora.class);
+        Intent ejer_2 = new Intent( this, listar.class);
         startActivity(ejer_2 );
+    }
+
+
+
+   @Override public boolean onCreateOptionsMenu(Menu mimenu){
+
+        getMenuInflater().inflate(R.menu.menu_nuevo,mimenu);
+        return true;
+    }
+
+   @Override public boolean onOptionsItemSelected(MenuItem opcionMenu) {
+        int id = opcionMenu.getItemId();
+        if (id == R.id.ListarContactos) {
+            Ejer_2(null);
+            return true;
+        }
+
+        if (id == R.id.AgregarContactos) {
+            Ejer_1(null);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(opcionMenu);
     }
 }
